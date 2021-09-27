be34ad57919c8fa30b37def91048baf81892b533
Salim Belim <sbelimdev@Salims-MacBook-Pro.local>
2021-09-27 22:45:11 +0100

    add pipeline update stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from '@aws-cdk/core';
 import {Artifact, Pipeline} from "@aws-cdk/aws-codepipeline";
-import {CodeBuildAction, GitHubSourceAction} from "@aws-cdk/aws-codepipeline-actions";
+import {
+    CloudFormationCreateUpdateStackAction,
+    CodeBuildAction,
+    GitHubSourceAction
+} from "@aws-cdk/aws-codepipeline-actions";
 import {SecretValue} from "@aws-cdk/core";
 import {BuildSpec, LinuxBuildImage, PipelineProject} from "@aws-cdk/aws-codebuild";
 
@@ -44,6 +48,18 @@ export class PipelineStack extends cdk.Stack {
                         },
                         buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
                     })
+                })
+            ]
+        })
+
+        pipeline.addStage({
+            stageName: 'Pipeline_Update',
+            actions: [
+                new CloudFormationCreateUpdateStackAction({
+                    actionName: "Pipeline_Update",
+                    stackName: "PipelineStack",
+                    templatePath: cdkBuildOutput.atPath('PipelineStack.template.json'),
+                    adminPermissions: true
                 })
             ]
         })
