cbc9245e06dac532bad1eff43efb098ce1c43360
Salim Belim <sbelimdev@Salims-MacBook-Pro.local>
2021-09-27 19:59:51 +0100

    add build stage with cdk

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,8 +1,53 @@
 import * as cdk from '@aws-cdk/core';
+import {Artifact, Pipeline} from "@aws-cdk/aws-codepipeline";
+import {CodeBuildAction, GitHubSourceAction} from "@aws-cdk/aws-codepipeline-actions";
+import {SecretValue} from "@aws-cdk/core";
+import {BuildSpec, LinuxBuildImage, PipelineProject} from "@aws-cdk/aws-codebuild";
 
 export class PipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const pipeline = new Pipeline(this, 'Pipeline', {
+            pipelineName: 'Pipeline',
+            crossAccountKeys: false
+        })
+
+        const sourceOutput = new Artifact('SourceOutput')
+
+        pipeline.addStage({
+            stageName: 'Source',
+            actions: [
+                new GitHubSourceAction({
+                    owner: 'sgbelim',
+                    repo: 'aws-pipeline',
+                    branch: 'main',
+                    actionName: 'Pipeline_Source',
+                    oauthToken: SecretValue.secretsManager('github-token'),
+                    output: sourceOutput
+                })
+            ]
+        })
+
+        const cdkBuildOutput = new Artifact('CdkBuildOutput')
+
+        pipeline.addStage({
+            stageName: 'Build',
+            actions: [
+                new CodeBuildAction({
+                    actionName: 'CDK_Build',
+                    input: sourceOutput,
+                    outputs: [cdkBuildOutput],
+                    project: new PipelineProject(this, 'CdkBuildProject', {
+                        environment: {
+                            buildImage: LinuxBuildImage.STANDARD_5_0
+                        },
+                        buildSpec: BuildSpec.fromSourceFilename('build-spec/cdk-build-spec.yml')
+                    })
+                })
+            ]
+        })
+
 
     }
 }
