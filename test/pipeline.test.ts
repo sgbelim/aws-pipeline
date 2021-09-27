c9d8ec46e24885e1751ed2fcff06d2a0637daf15
Salim Belim <sbelimdev@Salims-MacBook-Pro.local>
2021-09-27 22:33:07 +0100

    add tests in build pipeline

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,13 +1,15 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {SynthUtils} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Pipeline from '../lib/pipeline-stack';
 
 test('Empty Stack', () => {
+
+    // Arrange
     const app = new cdk.App();
-    // WHEN
+
+    // Act
     const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+    // Assert
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
